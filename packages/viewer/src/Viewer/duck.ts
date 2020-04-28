9c800ea74d3722c6d9fc6651f01bba9e9bd2b438
cadellchristo <c.christo@damstratechnology.com>
2020-04-28 21:51:19 +0800

    finished refactoring into redux-toolkit

--- packages/viewer/src/Viewer/duck.ts
+++ packages/viewer/src/Viewer/duck.ts
@@ -1,6 +1,5 @@
 import produce from "immer";
 import shortid from "shortid";
-import { debounce } from "debounce";
 
 import emptyState from "./emptyState.json";
 import { createAction, createReducer, PayloadAction } from "@reduxjs/toolkit";
@@ -10,21 +9,6 @@ import {
 	getIndexFromItem,
 	removeItemFromArray,
 } from "./array-util";
-
-export const INDENT = "INDENT";
-export const UNDENT = "UNDENT";
-export const MOVE_UP = "MOVE_UP";
-export const MOVE_DOWN = "MOVE_DOWN";
-export const UP = "UP";
-export const DOWN = "DOWN";
-export const RECORD_CHANGE = "RECORD_CHANGE";
-export const CALCULATION = "CALCULATION";
-
-export const up = () => ({ type: UP });
-export const down = () => ({ type: DOWN });
-
-export const moveUp = () => ({ type: MOVE_UP });
-export const moveDown = () => ({ type: MOVE_DOWN });
 
 type LoadArguments = {
 	data: ItemStore;
@@ -72,6 +56,26 @@ type UndentItemArguments = {
 };
 export const undentItem = createAction<UndentItemArguments>("UNDENT_ITEM");
 
+type MoveUpArguments = {
+	path: string[];
+};
+export const moveUp = createAction<MoveUpArguments>("MOVE_UP");
+
+type MoveDownArguments = {
+	path: string[];
+};
+export const moveDown = createAction<MoveDownArguments>("MOVE_DOWN");
+
+type UpArguments = {
+	path: string[];
+};
+export const up = createAction<UpArguments>("UP");
+
+type DownArguments = {
+	path: string[];
+};
+export const down = createAction<MoveDownArguments>("DOWN");
+
 export type ItemState = {
 	id: string;
 	content: string;
@@ -93,9 +97,11 @@ export const reducer = createReducer(emptyState, {
 		state.path = [];
 		state.item = action.payload.data;
 	},
+
 	[selectItem.type]: (state: State, action: any) => {
 		state.path = action.payload.path;
 	},
+
 	[editItem.type]: (state: State, action: any) => {
 		const { id, content } = action.payload;
 
@@ -109,6 +115,7 @@ export const reducer = createReducer(emptyState, {
 			item.content = content;
 		}
 	},
+
 	[addItem.type]: (state: State, action: any) => {
 		const { id, afterPath, content, children } = action.payload;
 
@@ -126,6 +133,7 @@ export const reducer = createReducer(emptyState, {
 		state.path.pop();
 		state.path.push(id);
 	},
+
 	[removeItem.type]: (state: State, action: any) => {
 		const { path } = action.payload;
 
@@ -139,16 +147,15 @@ export const reducer = createReducer(emptyState, {
 		deleteItem(id, state);
 
 		// select new item
+		// this isn't quite right, should use the same logic as UP
 		state.path.pop();
 		if (aboveItemId) {
 			state.path.push(aboveItemId);
 		}
 	},
+
 	[indentItem.type]: (state: State, action: IndentItemArguments) => {
-		const { id, parentId, collection } = getItemFromPath(
-			state.item,
-			state.path
-		);
+		const { id, collection } = getItemFromPath(state.item, state.path);
 
 		const newParentId = getIndexFromItem(collection, id, -1);
 		if (!newParentId) {
@@ -182,214 +189,108 @@ export const reducer = createReducer(emptyState, {
 
 		removeItemFromArray(collection, id);
 	},
-});
 
-const oldReducer = (state: State = emptyState, action: any) =>
-	produce(state, (draft: State) => {
-		const translator = new Translator(draft);
-		switch (action.type) {
-			case MOVE_UP: {
-				const target = translator.getCurrentItem();
-				const parent = translator.getParent(target.id);
+	[moveUp.type]: (state: State, action: MoveUpArguments) => {
+		const { id, index, collection } = getItemFromPath(state.item, state.path);
 
-				const currentIndex = parent.children.indexOf(target.id);
+		const aboveItemId = getIndexFromItem(collection, id, -1);
 
-				if (currentIndex === 0) {
-					break;
-				}
-
-				const aboveIndex = currentIndex - 1;
-				const aboveItemId = parent.children[aboveIndex];
-
-				parent.children[currentIndex] = aboveItemId;
-				parent.children[aboveIndex] = target.id;
-
-				break;
-			}
-			case MOVE_DOWN: {
-				const target = translator.getCurrentItem();
-				const parent = translator.getParent(target.id);
-
-				const currentIndex = parent.children.indexOf(target.id);
-
-				if (currentIndex === parent.children.length - 1) {
-					break;
-				}
-
-				const belowIndex = currentIndex + 1;
-				const belowItemId = parent.children[belowIndex];
-
-				parent.children[currentIndex] = belowItemId;
-				parent.children[belowIndex] = target.id;
-
-				break;
-			}
-			case UP: {
-				const currentItem = translator.getCurrentItem();
-				const aboveItem = translator.getAboveItem(currentItem.id);
-				draft.path.pop();
-
-				if (aboveItem != null) {
-					draft.path.push(aboveItem.id);
-
-					let children = aboveItem.children;
-					while (children.length > 0) {
-						const lastChildId = children.slice(-1)[0];
-						draft.path.push(lastChildId);
-
-						const lastChild = translator._getItemById(lastChildId);
-						children = lastChild.children;
-					}
-				}
-
-				break;
-			}
-			case DOWN: {
-				const currentItem = translator.getCurrentItem();
-
-				if (currentItem.children.length > 0) {
-					draft.path.push(currentItem.children[0]);
-				} else {
-					let belowItem = translator.getBelowItem(currentItem.id);
-					while (belowItem == null) {
-						const parent = translator.getParent(currentItem.id);
-						belowItem = translator.getBelowItem(parent.id);
-						draft.path.pop();
-					}
-					draft.path.pop();
-					draft.path.push(belowItem.id);
-				}
-
-				break;
-			}
-
-			case CALCULATION: {
-				const { id } = action.payload;
-				const item = translator._getItemById(id);
-				const parent = translator.getParent(item.id);
-
-				const calculation = item.content.replace("calculation: ", "");
-
-				try {
-					// (items, parent) => // [filteredItems]
-					// filteredItem = {(id), (content)}
-					const items = Object.keys(draft.item).map(
-						itemId => new EnhancedItem(itemId, draft.item)
-					);
-
-					// eslint-disable-next-line
-					const resultsFunction = eval(calculation);
-					const results = resultsFunction(items, parent);
-					item.children = results.map((result: EnhancedItem) => result.id);
-				} catch (e) {
-					item.error = e.message;
-				}
-				break;
-			}
-			default:
+		if (!aboveItemId) {
+			return;
 		}
-	});
 
-class EnhancedItem {
-	id: string;
-	itemStore: ItemStore;
-	itemState: ItemState;
+		collection[index - 1] = id;
+		collection[index] = aboveItemId;
+	},
 
-	constructor(id: string, itemStore: ItemStore) {
-		this.id = id;
-		this.itemStore = itemStore;
+	[moveDown.type]: (state: State, action: MoveDownArguments) => {
+		const { id, index, collection } = getItemFromPath(state.item, state.path);
 
-		this.itemState = itemStore[id];
-	}
+		const belowItemId = getIndexFromItem(collection, id, 1);
 
-	get content() {
-		return this.itemState.content;
-	}
+		if (!belowItemId) {
+			return;
+		}
 
-	get children() {
-		return this.itemState.children
-			.filter(childId => !!childId)
-			.map(childId => new EnhancedItem(childId, this.itemStore));
-	}
-}
+		collection[index + 1] = id;
+		collection[index] = belowItemId;
+	},
 
-// These are the happy methods
-class Translator {
-	state: State;
+	[up.type]: (state: State, action: UpArguments) => {
+		const { id, collection } = getItemFromPath(state.item, state.path);
+		let aboveItemId = getIndexFromItem(collection, id, -1);
 
-	constructor(draft: State) {
-		this.state = draft;
-	}
+		if (!aboveItemId) {
+			// select the parent
+			state.path.pop();
+			return;
+		}
 
-	getCurrentItem = () => {
-		const currentId = this._getCurrentId();
-		return this._getItemById(currentId);
-	};
+		state.path.pop();
+		state.path.push(aboveItemId);
 
-	getParent = (id: string) => {
-		const parentId = this._getParentId(id);
-		return this._getItemById(parentId);
-	};
+		const maxIterations = 100;
 
-	getAboveItem = (id: string) => {
-		return this._getAboveItems(id)[0];
-	};
+		let item = state.item[aboveItemId];
+		let iteration = 1;
 
-	getBelowItem = (id: string) => {
-		return this._getBelowItems(id)[0];
-	};
+		while (item && item.children.length > 0 && iteration < maxIterations) {
+			const lastChildId = getIndex(item.children, -1);
+			state.path.push(lastChildId);
+			item = state.item[lastChildId];
+			iteration++;
+		}
 
-	// these are the lower-level, private methods
-	_getItemById = (id: string) => {
-		return this.state.item[id];
-	};
+		if (iteration === maxIterations) {
+			console.error("maximum iterations reached for 'up'");
+		}
+	},
 
-	_getCurrentId = () => {
-		return [...this.state.path].reverse()[0];
-	};
+	[down.type]: (state: State, action: DownArguments) => {
+		let item = getItemFromPath(state.item, state.path);
 
-	_getAncestorIds = (id: string) => {
-		const reversedPath = [...this.state.path].reverse();
-		const indexOfId = reversedPath.indexOf(id);
-		// return everything in the path after the id
-		return reversedPath.slice(indexOfId + 1);
-	};
+		// check children
+		if (item.children.length > 0) {
+			state.path.push(item.children[0]);
+			return;
+		}
 
-	_getParentId = (id: string) => {
-		return this._getAncestorIds(id)[0];
-	};
+		// check siblings
+		let belowItemId = getIndexFromItem(item.collection, item.id, 1);
 
-	_getAboveItems = (id: string) => {
-		const parent = this.getParent(id);
-		const currentIndex = parent.children.indexOf(id);
-		const getAboveItemIds = parent.children.slice(0, currentIndex).reverse();
-		return getAboveItemIds.map(id => this._getItemById(id));
-	};
+		// check parents
+		while (!belowItemId && state.path.length > 1) {
+			state.path.pop();
+			item = getItemFromPath(state.item, state.path);
+			belowItemId = getIndexFromItem(item.collection, item.id, 1);
+		}
 
-	_getBelowItems = (id: string) => {
-		const parent = this.getParent(id);
-		const currentIndex = parent.children.indexOf(id);
-		const getBelowItemIds = [...parent.children].slice(currentIndex + 1);
-		return getBelowItemIds.map(id => this._getItemById(id));
-	};
-}
+		state.path.pop();
+		state.path.push(belowItemId);
+	},
+});
 
 function getItemFromPath(
 	itemStore: ItemStore,
 	path: string[]
 ): {
 	id: string;
+	index: number;
 	parentId: string;
 	parentsParentId: string;
 	collection: string[];
+	children: string[];
 } {
 	const id = getIndex(path, -1);
 	const parentId = getIndex(path, -2);
 	const parentsParentId = getIndex(path, -3);
 
-	const collection = getCollection(itemStore, parentId);
+	const collection = parentId ? getCollection(itemStore, parentId) : [];
+	const index = collection ? collection.indexOf(id) : -1;
 
-	return { id, parentId, parentsParentId, collection };
+	const children = itemStore[id].children;
+
+	return { id, index, parentId, parentsParentId, collection, children };
 }
 
 function getCollection(itemStore: ItemStore, parentId: string): string[] {
