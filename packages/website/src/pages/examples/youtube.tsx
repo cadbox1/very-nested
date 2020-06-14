1043edb208cae00cdee8002ab88db01725963255
cadellchristo <c.christo@damstratechnology.com>
2020-06-14 10:52:36 +0800

    updated the content on the website
    based on feedback I received from friends

--- packages/website/src/pages/examples/youtube.tsx
+++ packages/website/src/pages/examples/youtube.tsx
@@ -20,9 +20,30 @@ export default ({ data }: { data: any }) => {
 					px: 3,
 				}}
 			>
-				<Styled.h1 sx={{ fontSize: 4 }}>Youtube Example</Styled.h1>
-				<Styled.p sx={{ fontSize: 1 }}>
-					Copy and paste the id to use an existing item in a new list.
+				<Styled.h1 sx={{ fontSize: 5 }}>Youtube Example</Styled.h1>
+				<Styled.h3>Update a nested item</Styled.h3>
+				<Styled.p>
+					Expand the 'Recent' and <em>Matt Corby - Miracle Love</em> items,
+					click to the right of the YouTube link, hit enter, then add a
+					description and see it show up in the 'Music' and 'All' lists.
+				</Styled.p>
+				<Styled.h3>Nest an existing item in a new list</Styled.h3>
+				<Styled.p>
+					Click on <em>Babish's Huevos Rancheros</em> inside 'Cooking', copy the
+					id next to the edit box. Expand 'Recent', click on{" "}
+					<em>Spicy garlic fried chicken</em>, hit enter then paste the id in
+					the empty edit box.
+				</Styled.p>
+				<Styled.p>
+					Check out the{" "}
+					<Styled.a
+						href="https://cadbox1.github.io/very-nested-youtube/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						published page
+					</Styled.a>{" "}
+					if you like this list.
 				</Styled.p>
 				<ViewerContainer data={youtubeExample} readonly={false} />
 			</div>
--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -19,51 +19,130 @@ export default ({ data }) => {
 				}}
 			>
 				<Styled.h1 sx={{ fontSize: 6, mt: 8 }}>
-					Nested lists published on GitHub.
+					Create interesting lists on the internet.
 				</Styled.h1>
 				<Styled.p sx={{ fontSize: 3, mt: 4 }}>
 					Very Nested is a free and{" "}
 					<Styled.a
 						href="https://github.com/cadbox1/very-nested"
 						target="_blank"
+						rel="noopener noreferrer"
 					>
 						open source
 					</Styled.a>{" "}
-					tool to create nested lists and publish them GitHub. Each item can
-					belong to multiple lists and that has some cool results.
+					app to create interesting lists and publish them on the internet using
+					GitHub.
 				</Styled.p>
+				<Link
+					to="/get-started"
+					sx={{
+						fontSize: 1,
+						py: 3,
+						px: 4,
+						color: "background",
+						backgroundColor: "primary",
+						borderRadius: "0.25rem",
+						cursor: "pointer",
+						border: "none",
+						textDecoration: "none",
+					}}
+				>
+					Get Started
+				</Link>
 
 				<div>
-					<Styled.h2 sx={{ mt: 8 }}>Example</Styled.h2>
-					<Styled.a as={Link} to="/examples/youtube" sx={{ fontSize: 1 }}>
-						Edit this example.
-					</Styled.a>
+					<Styled.h2 sx={{ mt: 8 }}>Example - YouTube videos I like</Styled.h2>
+					<Styled.p>
+						Click on the plus icon to expand an item and see more details about
+						it. Each item can be in multiple lists, which means adding a
+						description to <em>Matt Corby's Miracle Love</em> in the 'Recent'
+						list will add it in the 'Music' and 'All' lists.
+					</Styled.p>
+					<Styled.p>
+						<Styled.a as={Link} to="/examples/youtube">
+							Edit this example
+						</Styled.a>{" "}
+						to see more. You can also check out the{" "}
+						<Styled.a
+							href="https://cadbox1.github.io/very-nested-youtube/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							published page
+						</Styled.a>{" "}
+						if you like this list.
+					</Styled.p>
 					<ViewerContainer data={youtubeExample} />
 				</div>
 
 				<div>
 					<Styled.h2 sx={{ mt: 8 }}>Features</Styled.h2>
-					<Styled.h3 sx={{ mt: 6 }}>One Item, many Lists</Styled.h3>
+					<Styled.h3 sx={{ mt: 6 }}>Nesting</Styled.h3>
 					<Styled.p>
-						Each item can belong to multiple lists so you can organise your
-						content in fun and creative ways.
+						Each item can be in multiple lists, so updating an item in one list
+						will update it across all lists. Items can also be expanded and
+						collapsed so you can organise your content in fun and creative ways.
 					</Styled.p>
 					<Styled.h3 sx={{ mt: 6 }}>No Lock-In</Styled.h3>
 					<Styled.p>
-						Very Nested is free, open source and publicly available on{" "}
+						Very Nested is free,{" "}
 						<Styled.a
 							href="https://github.com/cadbox1/very-nested"
 							target="_blank"
+							rel="noopener noreferrer"
+						>
+							open source
+						</Styled.a>{" "}
+						and we don't store any of your data. That means your content is safe
+						and under your control, now and into the future. Even if I stopped
+						working on Very Nested. You can even host your list on your own
+						domain like I've done for{" "}
+						<Styled.a
+							href="https://cooking.cadell.dev/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							cooking.cadell.dev
+						</Styled.a>
+						.
+					</Styled.p>
+					<Styled.h3 sx={{ mt: 6 }}>Published on GitHub</Styled.h3>
+					<Styled.p>
+						We use{" "}
+						<Styled.a
+							href="https://github.com/"
+							target="_blank"
+							rel="noopener noreferrer"
 						>
 							GitHub
-						</Styled.a>
-						. All your data is saved in your own GitHub account, under your
-						control.
-					</Styled.p>
-					<Styled.h3 sx={{ mt: 6 }}>Custom Domains</Styled.h3>
-					<Styled.p>
-						We integrate with GitHub pages to publish your content on your own
-						domain name or a github.com address.
+						</Styled.a>{" "}
+						to store your lists so you have more control over your data. We also
+						use their{" "}
+						<Styled.a
+							href="https://pages.github.com/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							hosting features
+						</Styled.a>{" "}
+						to create websites for your lists and their{" "}
+						<Styled.a
+							href="https://en.wikipedia.org/wiki/Version_control"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							version control
+						</Styled.a>{" "}
+						features to keep a history of changes to your content. GitHub is{" "}
+						<Styled.a
+							href="https://github.com/pricing"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							free
+						</Styled.a>{" "}
+						and the largest version control provider in the world with over 40
+						million users.
 					</Styled.p>
 				</div>
 			</div>
