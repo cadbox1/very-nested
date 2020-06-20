3d94c8efa3c7b7c8ed88999a3bfd588674b80f53
cadellchristo <c.christo@damstratechnology.com>
2020-06-20 16:47:55 +0800

    updated webpack config in viewer to handle font assets

--- packages/viewer/webpack.config.js
+++ packages/viewer/webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
 				use: "ts-loader",
 				exclude: /node_modules/,
 			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg)$/,
+				use: ["file-loader"],
+			},
 		],
 	},
 	resolve: {
