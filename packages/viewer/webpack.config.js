54c3925299991d47e85f78e1868b09a0d1ae32df
cadellchristo <c.christo@damstratechnology.com>
2020-06-21 18:56:27 +0800

    removed the css and file loaders from webpack

--- packages/viewer/webpack.config.js
+++ packages/viewer/webpack.config.js
@@ -11,14 +11,6 @@ module.exports = {
 				use: "ts-loader",
 				exclude: /node_modules/,
 			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.(eot|svg|ttf|woff|woff2|png|jpg)$/,
-				use: ["file-loader"],
-			},
 		],
 	},
 	resolve: {
